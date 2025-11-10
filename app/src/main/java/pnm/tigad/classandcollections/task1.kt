add37ac246888c4bfb1f283698ae7f94675e6df9
SyifaNurrohmahW <nurrohmahsyifa55@gmail.com>
2025-11-10 08:19:26 +0700

    Uncomment and define Event data class and main function

--- app/src/main/java/pnm/tigad/classandcollections/task1.kt
+++ app/src/main/java/pnm/tigad/classandcollections/task1.kt
@@ -1,20 +1,20 @@
 package pnm.tigad.classandcollections
 
-//data class Event (
-//    val title: String,
-//    val description : String? = null,
-//    val daypart: String,
-//    val durationInMinutes: Int)
-//
-//fun main(){
-//    val event = Event(
-//        title = "Study Kotlin",
-//        description = "Commit to studying Kotlin at least 15 per day .",
-//        daypart = "Evening",
-//        durationInMinutes = 15,
-//    )
-//    println(event)
-//}
+data class Event (
+    val title: String,
+    val description : String? = null,
+    val daypart: String,
+    val durationInMinutes: Int)
+
+fun main(){
+    val event = Event(
+      title = "Study Kotlin",
+      description = "Commit to studying Kotlin at least 15 per day .",
+      daypart = "Evening",
+      durationInMinutes = 15,
+    )
+  println(event)
+}
 
 
 
